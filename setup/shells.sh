e5415e1fd8c83853ee2696794646bff0d432e9f7
lendenmc <chris@chrisdeveloper.com>
2016-12-29 11:28:12 +0100

    Fix shells.sh bugs on Windows (no 'sudo', 'chsh', etc)

--- setup/shells.sh
+++ setup/shells.sh
@@ -5,16 +5,26 @@
 # add shells to /etc/shells
 shells="zsh bash dash fish ksh pdksh xonsh"
 for shell in $shells; do
-	_test_executable "$shell" || return 1
+	if ! _test_executable "$shell" 2>/dev/null; then
+		printf "Shell %s is not installed\n" "$shell"
+		continue
+	fi
 	shell_path="$(command -v "$shell")"
 	if grep -Fqx "$shell_path" /etc/shells; then
-		printf "Shell %s is already installed\n" "$shell_path"
+		printf "Shell %s is already in /etc/shells\n" "$shell_path"
 	else
 		printf "Adding shell %s to /etc/shells\n" "$shell_path"
-		printf "%s\n" "$shell_path"	\
-			| sudo tee -a /etc/shells >/dev/null
+		if command -v sudo >/dev/null 2>&1; then
+			printf "%s\n" "$shell_path"	\
+				| sudo tee -a /etc/shells >/dev/null
+		else
+			printf "%s\n" "$shell_path"	\
+				| tee -a /etc/shells >/dev/null
+		fi
 	fi
 done
 
 # switch default shell to zsh
-_test_executable "zsh" 2>/dev/null && chsh -s "$(command -v zsh)"
+if command -v chsh >/dev/null 2>&1; then
+	_test_executable "zsh" 2>/dev/null && chsh -s "$(command -v zsh)"
+fi
