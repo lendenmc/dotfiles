8e57a2d2c47977d22c73ca1fe73695171016c35b
lendenmc <chris@chrisdeveloper.com>
2025-11-19 07:18:56 +0100

    Stop pipx complaining about path in python setup script

--- setup/python/setup.sh
+++ setup/python/setup.sh
@@ -5,6 +5,7 @@
 
 # pip-install standalone python applications in dedicated virtualenvs with pipx
 _test_executable "pipx" || exit 1
+pipx ensurepath
 pipx_packages=./python/pipx_packages.txt
 _parse_text_file "$pipx_packages"	\
 	| while read -r package options; do
