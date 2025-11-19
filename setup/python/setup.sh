da7ad49fc4809b6f8ac7de30f02938b903df5971
lendenmc <chris@chrisdeveloper.com>
2025-11-19 08:04:10 +0100

    Stop pipx complaining for already-installed packages

--- setup/python/setup.sh
+++ setup/python/setup.sh
@@ -6,8 +6,11 @@
 # pip-install standalone python applications in dedicated virtualenvs with pipx
 _test_executable "pipx" || exit 1
 pipx ensurepath
+export PIPX_HOME="$HOME"/.local/pipx
 pipx_packages=./python/pipx_packages.txt
 _parse_text_file "$pipx_packages"	\
 	| while read -r package options; do
+		package_dir="${PIPX_HOME}/venvs/${package%%[*}"
+		_test_program_folder "$package" "package" "$package_dir" || continue
 		eval "pipx install $options $package" || exit 1
 	done
