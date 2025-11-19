0438de683710523fe6c10a81aa78c83580e5b31f
lendenmc <chris@chrisdeveloper.com>
2025-11-19 07:15:09 +0100

    Stop upgrading python default packages as homebrew python blocks global pip installs

--- setup/python/setup.sh
+++ setup/python/setup.sh
@@ -3,21 +3,6 @@
 # shellcheck disable=SC1091
 . ./utils.sh || exit 1
 
-unset PIP_REQUIRE_VIRTUALENV
-
-if _test_executable "pip3"
-then
-	pip_name="pip3"
-else
-	_test_executable "pip" || exit 1
-	pip_name="pip"
-fi
-
-printf "Installing or upgrading global python packages\n"
-
-# upgrade python 3's default packages that come with pip
-eval "$pip_name install --upgrade pip setuptools wheel" || exit
-
 # pip-install standalone python applications in dedicated virtualenvs with pipx
 _test_executable "pipx" || exit 1
 pipx_packages=./python/pipx_packages.txt
