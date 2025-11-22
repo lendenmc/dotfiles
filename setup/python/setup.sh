89bde16ed12d44982569dd9d45ac68500c53a9c8
lendenmc <chris@chrisdeveloper.com>
2025-11-22 08:30:16 +0100

    Fix pipx install warning about its path availabilty

--- setup/python/setup.sh
+++ setup/python/setup.sh
@@ -5,8 +5,9 @@
 
 # pip-install standalone python applications in dedicated virtualenvs with pipx
 _test_executable "pipx" || exit 1
-pipx ensurepath
-export PIPX_HOME="$HOME"/.local/pipx
+export PIPX_HOME="${HOME}/.local/pipx"
+export PIPX_BIN_DIR="${HOME}/.local/bin"
+export PATH="$PIPX_BIN_DIR:$PATH"
 pipx_packages=./python/pipx_packages.txt
 _parse_text_file "$pipx_packages"	\
 	| while read -r package options; do
