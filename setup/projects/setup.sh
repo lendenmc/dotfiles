2be391c45da94ca649e744beb0f1e60d746fbcde
lendenmc <chris@chrisdeveloper.com>
2025-11-23 14:26:14 +0100

    Remove broken and obscure optional directory and option parsing from project clone script

--- setup/projects/setup.sh
+++ setup/projects/setup.sh
@@ -9,22 +9,15 @@ set -eu
 
 projects=./projects/projects.txt
 _parse_text_file "$projects"	\
-	| while read -r project options; do
+	| while read -r project; do
 		project_name=${project##*/}
-		project_dir="${HOME}/projects/${project_name}"
-		optional_dir=$(printf "%s" "$options" | GREP_OPTIONS="" command grep -Eo "^\\\$\S*")
-		if [ -n "$optional_dir" ]; then
-			eval "optional_dir=$optional_dir"
-			! _test_empty_dir "$optional_dir" || continue
-			options=$(printf "%s" "$optional_dir" | cut -d' ' -f 2-)
-			cmd="git clone $project \"$optional_dir\" $options"
-			printf "Cloning remote git repository %s\n" "$project"
-		else
-			mkdir -p "$project_dir"
-			! _test_empty_dir "$project_dir" || continue
-			cmd="git clone $project \"$project_dir\" $options"
-			printf "Cloning remote git repository %s\n" "$project_name"
-		fi
-		eval "$cmd"
-		printf "\n"
+		project_dir="$HOME/projects/$project_name"
+	
+	    mkdir -p "$project_dir"
+	    ! _test_empty_dir "$project_dir" || continue
+	
+	    printf "Cloning remote git repository %s\n" "$project_name"
+	    git clone "$project" "$project_dir"
+	
+	    printf "\n"
 	done
